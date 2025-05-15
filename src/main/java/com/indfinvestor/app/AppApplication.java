9ee1bb779c066fec3e242aed44fdb66459275adc
Jubin Jayaprakash <jaypac@gmail.com>
2025-05-15 19:24:39 +0530

    Init Setup

---
+++ src/main/java/com/indfinvestor/app/AppApplication.java
@@ -0,0 +1,14 @@
+package com.indfinvestor.app;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
